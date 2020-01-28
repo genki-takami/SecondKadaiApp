353c48c9ce2343553e562ac49b02eb8ea2a5ad33
genki-takami <takamigenki7@gmail.com>
2020-01-29 02:49:24 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 髙見元基 on 2020/01/29.
+//  Copyright © 2020 genki-takami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
